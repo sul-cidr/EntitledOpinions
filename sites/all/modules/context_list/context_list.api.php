5fa9204eb7cea2104f6cc3b33340094919e40d30
John Bickar <jbickar@stanford.edu>
2014-10-17 10:06:38 -0700

    Updated module context_list-7.x-1.x-dev (6d81d058)

--- sites/all/modules/context_list/context_list.api.php
+++ sites/all/modules/context_list/context_list.api.php
@@ -1,8 +1,13 @@
 <?php
+/**
+ * @file
+ * API hooks
+ */
 
 /**
  * Register a condition display class
- * @return array An array of display PHP classes that are used to display conditions
+ * @return array
+ *   An array of display PHP classes that are used to display conditions
  */
 function hook_context_list_register_condition_display() {
   return array(
@@ -14,11 +19,30 @@ function hook_context_list_register_condition_display() {
 
 /**
  * Register a reaction display class
- * @return array An array of display PHP classes that are used to display conditions
+ * @return array
+ *   An array of display PHP classes that are used to display conditions
  */
 function hook_context_list_register_reaction_display() {
   return array(
     'all' => 'ContextListReactionDisplay',
     'block' => 'ContextListReactionDisplay_block',
   );
+}
+
+/**
+ * Alter a block name
+ */
+function hook_context_list_reaction_block_name(&$block_name, &$details) {
+}
+
+/**
+ * Alter the list of blocks available to the context display plugin
+ */
+function hook_context_list_reaction_blocks(&$blocks) {
+}
+
+/**
+ * Alter a theme name
+ */
+function hook_context_list_reaction_theme_name(&$theme_name, &$regions) {
 }
