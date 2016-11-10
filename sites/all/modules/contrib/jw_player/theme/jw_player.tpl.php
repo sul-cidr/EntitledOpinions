770c3ea0253415a4f661a3eaf5c31e6d0acd1c99
Kellie Brownell <kbrownel@stanford.edu>
2016-11-09 16:02:41 -0800

    Update module jw_player-7.x-1.0-beta2

--- sites/all/modules/contrib/jw_player/theme/jw_player.tpl.php
+++ sites/all/modules/contrib/jw_player/theme/jw_player.tpl.php
@@ -5,19 +5,13 @@
  *
  * Variables available:
  * - $html_id: Unique id generated for each video.
- * - $width: Width of the video player.
- * - $height: Height of the video player.
- * - $file_url: The url of the file to be played.
  * - $jw_player_inline_js_code: JSON data with configuration settings for the video player.
- * - $poster: URL to an image to be used for the poster (ie. preview image) for this video.
  *
  * @see template_preprocess_jw_player()
  */
 ?>
-<div class="jwplayer-video">
-  <video id="<?php print $html_id ?>" width="<?php print $width ?>" height="<?php print $height ?>" controls="controls" preload="none"<?php if(isset($poster)) : ?> poster="<?php print $poster ?>"<?php endif ?>>
-    <source src="<?php print $file_url ?>"<?php if (isset($file_mime)): ?> type="<?php print $file_mime ?>"<?php endif ?> />
-  </video>
+<div id="<?php print $html_id ?>" class="jwplayer-video">
+Loading Video...
 </div>
 <?php if(isset($jw_player_inline_js_code)): ?>
   <script type="text/javascript">
