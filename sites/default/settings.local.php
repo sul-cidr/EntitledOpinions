471ca3db041c6f30ebe1230c36485c68cb9a51f1
Scott Bailey <csb5t@virginia.edu>
2018-04-27 09:13:15 -0700

    Refactor to use docksal; update drupal

--- sites/default/settings.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-# Stanford - Set different session cookies for HTTP and HTTPS.
-if (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS'])) {
-   ini_set('session.cookie_secure', 1);
-}
-
-$conf['allow_authorize_operations'] = FALSE;
-$conf['image_allow_insecure_derivatives'] = TRUE;
